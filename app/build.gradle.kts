eb079d871ad73dad7a21a79ea4356b8d3566354d
Tejas <thimmaiah.k@stfranciscollege.edu.in>
2026-06-11 15:34:02 +0530

    playstroe first official launch version 4(1.1.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.thimmaiah.ebors"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
